f4c76050c366113aafb178b474de5398daa0a21c
Jerry Howard <jerryhoward@gmail.com>
2015-11-29 17:25:47 -0500

    Added the custom 'wrap-version' middleware, verified that it is working. Also added an external 'home-handler' handler function.

--- src/blaagh/core.clj
+++ src/blaagh/core.clj
@@ -4,8 +4,16 @@
             [ring.middleware.reload :as reload]
             [ring.middleware.defaults :refer :all]))
 
+(defn home-handler
+    [request]
+    (str "Blaagh version " (:app-version request)))
+
+(defn wrap-version [handler]
+    (fn [request]
+        (handler (assoc request :app-version "0.1"))))
+
 (defroutes all-routes
-    (GET "/" [] "Hello World!\n")
+    (GET "/" [request] home-handler)
     ; (GET "/" [] "Show something")
     (POST "/" [] "Create something")
     (PUT "/" [] "Replace something")
@@ -15,4 +23,5 @@
     (HEAD "/" [] "Preview something"))
 
 (defn -main []
-  (run-server (reload/wrap-reload #'all-routes) {:port 5000}))
+    (run-server (reload/wrap-reload (wrap-version #'all-routes)) {:port 5000}))
+    ; (run-server (reload/wrap-reload #'all-routes) {:port 5000}))
