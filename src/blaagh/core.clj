cb029f5b2c84c19fc241764bf7cc62feda4ff6b5
Jerry Howard <jerryhoward@gmail.com>
2015-11-29 17:54:32 -0500

    Added the 'wrap-spy' custom handler from that one blog post; it automatically outputs pretty-printed Request and Response maps in the server console. Confirmed working.

--- src/blaagh/core.clj
+++ src/blaagh/core.clj
@@ -4,6 +4,8 @@
             [ring.middleware.reload :as reload]
             [ring.middleware.defaults :refer :all]))
 
+(require 'clojure.pprint)
+
 (defn home-handler
     [request]
     (str "Blaagh version " (:app-version request)))
@@ -11,6 +13,18 @@
 (defn wrap-version [handler]
     (fn [request]
         (handler (assoc request :app-version "0.1"))))
+
+(defn wrap-spy [handler]
+    ""
+    (fn [request]
+        (println "-------------------------------")
+        (println "Incoming Request Map:")
+        (clojure.pprint/pprint request)
+        (let [response (handler request)]
+            (println "Outgoing Response Map:")
+            (clojure.pprint/pprint response)
+            (println "-------------------------------")
+            response)))
 
 (defroutes all-routes
     (GET "/" [request] home-handler)
@@ -23,5 +37,9 @@
     (HEAD "/" [] "Preview something"))
 
 (defn -main []
-    (run-server (reload/wrap-reload (wrap-version #'all-routes)) {:port 5000}))
+    (run-server 
+        (reload/wrap-reload 
+            (wrap-spy 
+                (wrap-version #'all-routes))) {:port 5000}))
+                    ; (wrap-defaults #'all-routes site-defaults)))) {:port 5000}))
     ; (run-server (reload/wrap-reload #'all-routes) {:port 5000}))
