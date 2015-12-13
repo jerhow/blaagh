185624ef16d42b1a7a712b20aaf0c96982afe974
Jerry Howard <jerryhoward@gmail.com>
2015-12-13 18:24:01 -0500

    A basic migration demo that creates three tables

--- src/blaagh/db/core.clj
+++ src/blaagh/db/core.clj
@@ -4,10 +4,19 @@
 (def db-conn {
     :classname "org.sqlite.JDBC"
     :subprotocol "sqlite"
-    :subname "resources/db/blaagh.sqlite3"})
+    :subname "resources/db/blaagh.sqlite3"}) ;; creates DB if not already present
 
 (defn get-names []
     (let [results (sql/query db-conn ["select name from foo"])]
         (cond
             (empty? results) {:status 404}
             :else results)))
+
+(defn create-tables []
+    (try 
+        (sql/db-do-commands db-conn 
+            "CREATE TABLE foo (name text); "
+            "CREATE TABLE bar (name text); "
+            "CREATE TABLE baz (name text); ")
+        (catch Exception e 
+            (println e))))
